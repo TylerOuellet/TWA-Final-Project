05ca65b34cd3bdf5d989002c24f8c85e5248b8c2
Jade03 <LE2026334@crc-lennox.qc.ca>
2024-05-07 08:52:21 -0400

    starting

---
+++ client/Views/Part1.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import Landingpage from '../Landingpage';
+
+function Part1() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            navigate('/');
+        }, 2000);
+
+        return () => clearTimeout(timeout);
+    }, [navigate]);
+
+    return (
+        <div>
+            {/* */}
+            <Landingpage />
+        </div>
+    );
+}
+
+export default Part1;
---
+++ client/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.html';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Part1 from "./Views/Part1.tsx"
+// import Part2 from "./Views/Part2.tsx";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Part1 />} />
+        {/* <Route path="/part2" element={<Part2 />} /> */}
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+);
