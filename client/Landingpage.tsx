525092404bb43e3cfdee378126b234e558de915a
Jade03 <LE2026334@crc-lennox.qc.ca>
2024-05-07 09:03:45 -0400

    Prototype

--- client/Landingpage.tsx
+++ client/Landingpage.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 // import './index.html'; // css file
-import image from 'images/LandingPage.png';
+import image from '/images/LandingPage.png';
 
 const LandingPage = () => {
-    return (
-        <div className="container">
-          <div className='row mt-5'>
-            <div className="col-md-6 text-center">
-              <img src={image} alt="Landing" className="img-fluid" />
-            </div>
-          </div>
-          <div className="row mt-3">
-            <div className="col-md-6 text-center">
-              <h2>Landing Page</h2>
-              <p>HEEEEEEEELLLLOOOOOOOO</p>
-              <button type="button" className="btn btn-primary">Click</button>
-            </div>
-          </div>
+  return (
+    <div className="container text-center">
+      <div className='row'>
+        <div className="text-center">
+          <img src={image} alt="Landing" className="img-fluid" />
         </div>
-    );
+      </div>
+      <div className="row">
+        <div className="text-center">
+          <h2>Landing Page</h2>
+          <p>HEEEEEEEELLLLOOOOOOOO WELLLLCOMMEEE</p>
+          <button type="button" className="btn btn-primary">Click</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
