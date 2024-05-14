7d10cce9ae85ba86b03d015d72dd658efc878068
MisterArun <78886361+MisterArun@users.noreply.github.com>
2024-05-14 14:43:40 -0400

    Second graph done

--- database/second_graph.py
+++ database/second_graph.py
@@ -1,43 +1,59 @@
-# imports
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 from pymongo import MongoClient
 
 # mongodb connection setup
+cluster_uri = "mongodb+srv://tylerouellet93:YmataxFAcXo3GqlW@cluster0.9xoenc5.mongodb.net/"
 client = MongoClient("mongodb://localhost:27017")
 db = client['project']
-collection = db['test2']
+collection = db['data']
 
-def second_graph(country):
-    query = {"country": country}
+def pie_charts(year, *countries):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
     
-    projection = {
-        "year": 1,
-        "hydro_consumption": 1,
-        "solar_consmuption": 1,
-        "biofuel_consumption": 1,
-        "wind_consumption": 1
-        # "_id": 0
-    }
+    file_name = "second_graph.png"
     
-    cursor = collection.find(query, projection)
+    axs = axs.flatten()
 
-    df = pd.DataFrame(list(cursor))
-    print(df["hydro_consumption"])
-
-    plt.figure(1)
-    plt.pie(df["solar_consmuption"])
-
-    plt.title(f'Sustainable Energy Distribution by country')
-    plt.grid(True)
-    plt.legend()
+    explode = [.03, .03, .03, .03]
+    
+    for ax, country in zip(axs.flatten(), countries):
+        
+        query = {"country": country, "year": year}
+        projection = {
+            "hydro_consumption": 1,
+            "solar_consumption": 1,
+            "biofuel_consumption": 1,
+            "wind_consumption": 1,
+            "_id": 0
+        }
+        cursor = collection.find_one(query, projection)
+        
+        if cursor:
+            data = list(cursor.values())
+            # print(f"Data for {country}: {data}")
+            ax.pie(data, autopct='%1.1f%%', explode=explode)
+            ax.set_title(f"{country}")
+        else:
+            print(f"no data for {country}")
+    if len(countries) < 4:
+        for j in range(1, 4):
+            axs[j].axis('off')
+    if cursor:
+        legend_labels = ["Biofuel", "Hydro", "Solar", "Wind"]
+        fig.legend(legend_labels, title="Energy Types", loc="upper right")
+        
+    fig.suptitle(f"Sustainable Energy Consumption Distribution by Country ({year})", fontsize=15,)
     plt.tight_layout()
-    plt.show()
+    # plt.show()
+    plt.savefig(f"../server/output/{file_name}")
 
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("python script.py year country1 country2")
+        sys.exit(1)
 
-
-country = sys.argv[1]
-# year = sys.argv[2]
-
-second_graph(country)
+    year = int(sys.argv[1])
+    countries = sys.argv[2:]
+    pie_charts(year, *countries)
